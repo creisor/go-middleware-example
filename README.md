055ebcff2bc9ffa3c7c240d1f3a68d4d20453edd
Chris Reisor <chris.reisor@getbraintree.com>
2019-11-05 17:58:19 +0000

    route specific middleware works

--- README.md
+++ README.md
@@ -5,14 +5,18 @@ To start:
 
     go run main.go
 
-To be authorized:
+The home route requires no authentication:
 
-    curl -v -H "auth: notahacker" http://localhost:3000/ 
+    curl -v http://localhost:3000/
 
-To be denied:
+To reach the `/authorized` route, you must be authorized with your super cool token: `notahacker`:
 
-    curl -v -H "auth: imahacker" http://localhost:3000/
+    curl -v -H "auth: notahacker" http://localhost:3000/authorized
+
+To be denied to `/authorized`:
+
+    curl -v -H "auth: totalhacker" http://localhost:3000/authorized
 
 or
 
-    curl -v http://localhost:3000/
+    curl -v http://localhost:3000/authorized
--- main.go
+++ main.go
@@ -11,22 +11,44 @@ import (
 
 func main() {
 	router := mux.NewRouter()
+	authRouter := mux.NewRouter().PathPrefix("/authorized").Subrouter().StrictSlash(true)
+
 	router.HandleFunc("/", homeHandler)
-	n := negroni.Classic() // Includes some default middlewares
-	n.Use(negroni.HandlerFunc(verifyMiddleware))
+
+	router.PathPrefix("/authorized").Handler(negroni.New(
+		NewVerifyMiddleware("notahacker"),
+		&AuthHandler{},
+		negroni.Wrap(authRouter),
+	))
+
+	n := negroni.Classic()
 	n.UseHandler(router)
 
 	http.ListenAndServe(":3000", n)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello")
+	fmt.Fprintf(w, "Hello from home")
 }
 
-func verifyMiddleware(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+type AuthHandler struct{}
+
+func (h *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Fprintf(w, "You are authorized")
+}
+
+type VerifyMiddleware struct {
+	key string
+}
+
+func NewVerifyMiddleware(key string) *VerifyMiddleware {
+	return &VerifyMiddleware{key}
+}
+
+func (v *VerifyMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	auth := r.Header.Get("auth")
 	log.Printf("auth: %s", auth)
-	if auth == "notahacker" {
+	if auth == v.key {
 		next(w, r)
 		return
 	}
